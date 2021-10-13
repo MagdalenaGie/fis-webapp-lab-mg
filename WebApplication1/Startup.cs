380a08f07e8f2791b93e99fb7c77f97bf0f8168a
Magdalena Górska <magdalena.gorska@relativity.com>
2021-10-13 13:03:03 +0200

    commit with variabe

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -28,12 +29,14 @@ namespace WebApplication1
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
+            app.UseEndpoints(endpoints => 
+            { 
+                endpoints.MapGet("/", async context => 
                 {
-                    await context.Response.WriteAsync("Hello World from CI/CD, see the difference!");
-                });
+                    var configuration = context.RequestServices.GetService<IConfiguration>(); 
+                    string testVariableValue = configuration.GetSection("test")?.Value ?? "test not configured!"; 
+                    await context.Response.WriteAsync("Test variable:" + testVariableValue); 
+                }); 
             });
         }
     }
